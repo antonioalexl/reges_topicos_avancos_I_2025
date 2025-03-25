3c2ec9e7ca7ca92dac29d23bc6406f93018bfb91
Alex Lopes <alex.lopes@2morrowsports.com.br>
2025-03-25 20:23:47 -0300

    ajustes

--- reges-farm/src/app/app.routes.ts
+++ reges-farm/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'animais', loadComponent: () => import('./animais/animais.component').then(m => m.AnimaisComponent) },
+  { path: 'animais/animal', loadComponent: () => import('./animais/animal/animal.component').then(m => m.AnimalComponent) },
+  { path: 'animais/animal:id', loadComponent: () => import('./animais/animal/animal.component').then(m => m.AnimalComponent) },
+
+
+];
