dc6e5f6131567e35e562470d8a9cd4c5b309eef9
Alex Lopes <alex.lopes@2morrowsports.com.br>
2025-03-25 21:22:49 -0300

    ajuste

--- reges-farm/src/app/animais/animal/animal.component.ts
+++ reges-farm/src/app/animais/animal/animal.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Animal } from '../../modelos/animal';
 
 @Component({
   selector: 'app-animal',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './animal.component.html',
   styleUrl: './animal.component.css'
 })
 export class AnimalComponent {
 
+  protected animal: Animal = {};
 }
