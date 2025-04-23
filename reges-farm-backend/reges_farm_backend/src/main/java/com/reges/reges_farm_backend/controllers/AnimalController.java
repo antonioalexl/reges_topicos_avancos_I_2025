7c76b9afaa59a27329f7fb59d291d22212df6bc5
Alex Lopes <alex.lopes@2morrowsports.com.br>
2025-04-22 21:49:50 -0300

    backend

--- reges-farm-backend/reges_farm_backend/src/main/java/com/reges/reges_farm_backend/controllers/AnimalController.java
+++ reges-farm-backend/reges_farm_backend/src/main/java/com/reges/reges_farm_backend/controllers/AnimalController.java
@@ -1,5 +1,47 @@
 package com.reges.reges_farm_backend.controllers;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.reges.reges_farm_backend.models.Animal;
+import com.reges.reges_farm_backend.repositories.AnimalRepository;
+
+
+
+
+@RestController
 public class AnimalController {
 
+       
+    @GetMapping("animais")
+    public List<Animal> GetAnimais() throws SQLException{
+        AnimalRepository repository = new AnimalRepository();
+        return repository.selectAll();
+    }
+
+    @PostMapping("animal")
+    public void InserirAnimal(@RequestBody Animal entity) throws SQLException {
+        AnimalRepository repository = new AnimalRepository();
+        repository.insert(entity);        
+       
+    }
+    @GetMapping("animal/{id}")
+    public Animal GetAnimalById(@PathVariable(value = "id")int id) throws SQLException {
+        AnimalRepository repository = new AnimalRepository();
+        var animal = repository.selectById(id);
+        if(animal != null){
+            return animal;
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "");
+     
+    }
+    
 }
